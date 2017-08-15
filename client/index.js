83d2845a8de11b3880ee465f69a82a565e6e8256
sw-yx <swyx@swyx.io>
2017-08-15 12:58:12 -0400

    sldkj

--- client/index.js
+++ client/index.js
@@ -5,7 +5,7 @@ const buildMarker = require("./marker");
   * Instantiate the Map
   */
 
-mapboxgl.accessToken = "---- INSERT YOUR ACCESS TOKEN HERE ----";
+mapboxgl.accessToken = "pk.eyJ1Ijoic3d5eCIsImEiOiJjajY4M2hvcGYwY3lrMnZueXB2dDg5cWt5In0.7HkydSCJMjYLqxIxq5a-5A";
 const map = new mapboxgl.Map({
   container: "map-canvas",
   center: [-74.0, 40.731],
